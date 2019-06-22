eb0031521c8d08d0d85ab0180c0bd909ed258280
Prabhat Sharma <hi.prabhat@gmail.com>
2019-06-22 10:34:23 -0700

    expanded commands

--- k8s-1.15-kubeadm/setup-node.sh
+++ k8s-1.15-kubeadm/setup-node.sh
@@ -20,9 +20,12 @@ apt-mark hold kubelet kubeadm kubectl
 
 # install docker
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - add-apt-repository \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable" apt-get update apt-get install docker-ce docker-ce-cli containerd.io
+   stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+
 usermod -a -G docker $USER
-reboot
